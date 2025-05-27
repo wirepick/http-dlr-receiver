b602d5d02e6f0b2f70e7060711d2896da3a6ebdb
Fousse-Douc <fdoucfousse@gmail.com>
2025-05-27 10:31:30 +0000

    first commit

---
+++ src/main/java/com/wirepick/http_dlr_receiver/HttpDlrReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.wirepick.http_dlr_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpDlrReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpDlrReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wirepick/http_dlr_receiver/HttpDlrReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirepick.http_dlr_receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpDlrReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
